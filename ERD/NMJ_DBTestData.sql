6e1d42bffc507d204293d9b49d9ed08b03381c12
user <user@DESKTOP-HAGABK2>
2020-02-11 18:03:45 +0900

    성일 : db 드랍 SQL문 추가

---
+++ ERD/NMJ_DBTestData.sql
@@ -0,0 +1,4 @@
+insert into nmj_member (mb_id, mb_pw, mb_name, mb_img_org, mb_img_sav, mb_tel, mb_email, mb_type)
+values ('ksi', '1234', '강성일', null, null, '010-4011-3450', 'ksi0678@naver.com', 1);
+
+select * from nmj_member nm ;
