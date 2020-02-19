621f82668be6256d7e1cd79409f1ee1a450fac2a
mgb97 <mgb97@DESKTOP-GiBeom>
2020-02-20 03:47:43 +0900

    기범 : 매장찾기 페이지

--- src/main/java/qna/project/nmj/beans/dao/MemberDAO.java
+++ src/main/java/qna/project/nmj/beans/dao/MemberDAO.java
@@ -1,9 +1,12 @@
 package qna.project.nmj.beans.dao;
 
 import qna.project.nmj.beans.MemberDTO;
+import qna.project.nmj.beans.StoreDTO;
 
 public interface MemberDAO {
 	public MemberDTO selectId(String mb_name, String mb_email);
 	public MemberDTO selectPw(String mb_id, String mb_name, String mb_email);
 	public int updatePw(String mb_pw, String mb_id);
+	
+	public StoreDTO selectStore(String store_type);
 }
