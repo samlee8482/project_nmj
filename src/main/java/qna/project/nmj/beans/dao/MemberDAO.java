5dfc33416870c67a4315cc1cfc6f94b66b9f9231
mgb97 <mgb97@DESKTOP-GiBeom>
2020-02-21 00:27:46 +0900

    기범 : ajax pull 받을래용

--- src/main/java/qna/project/nmj/beans/dao/MemberDAO.java
+++ src/main/java/qna/project/nmj/beans/dao/MemberDAO.java
@@ -10,5 +10,8 @@ public interface MemberDAO {
 	public MemberDTO selectPw(String mb_id, String mb_name, String mb_email);
 	public int updatePw(String mb_pw, String mb_id);
 	
-	public ArrayList<StoreDTO> selectStore(int store_type);
+	// 몇번째 from 부터 몇개 rows 를 SELECT
+	public ArrayList<StoreDTO> selectStore(int store_type, int from, int rows);
+	
+	
 }
