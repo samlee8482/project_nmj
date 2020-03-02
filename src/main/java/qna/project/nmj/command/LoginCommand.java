6e2c79f0b784ab9dc905c98cabb196d8ece1d921
user <user@DESKTOP-HAGABK2>
2020-03-02 15:25:38 +0900

    성일 : 세션 작업

--- src/main/java/qna/project/nmj/command/LoginCommand.java
+++ src/main/java/qna/project/nmj/command/LoginCommand.java
@@ -1,28 +1,24 @@
 package qna.project.nmj.command;
 
-import java.util.Map;
-
 import org.springframework.ui.Model;
 
 import qna.project.nmj.beans.C;
-import qna.project.nmj.beans.MemberDTO;
 import qna.project.nmj.beans.dao.MemberDAO;
 import qna.project.nmj.security.PasswordEncoding;
 
-public class SignUpCustomerOkCommand implements Command {
+public class LoginCommand implements Command {
 
 	@Override
 	public void execute(Model model) {
-		int cnt = 0;
 		PasswordEncoding pe = new PasswordEncoding();
-		Map<String, Object> map = model.asMap();
-		MemberDTO dto = (MemberDTO)map.get("dto");
-		dto.setMb_pw(pe.encode(dto.getMb_pw()));
+		String mb_id = (String)model.getAttribute("mb_id");
+		String mb_pw = (String)model.getAttribute("mb_pw");
 		MemberDAO dao = C.sqlSession.getMapper(MemberDAO.class);
-		
-		cnt = dao.insertMember(dto);
-		
-		model.addAttribute("result", cnt);
+		if(pe.matches(mb_pw, dao.login(mb_id))) {
+			model.addAttribute("mb_uid", dao.getMbUid(mb_id));
+		}else {
+			model.addAttribute("mb_uid", 0);
+		}
 	}
 
 }
